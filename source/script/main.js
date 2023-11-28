097f722ab600deabb618dbf863b47359e631c7b5
Oleg Tsyganov <redsmoke19@gmail.com>
2023-11-28 10:47:35 +0400

    first commit

---
+++ source/script/main.js
@@ -0,0 +1,11 @@
+import "../scss/style.scss"
+import "./utils/prism.js";
+import {iosVhFix} from "./utils/ios-vh-fix.js";
+
+window.addEventListener('DOMContentLoaded', () => {
+  iosVhFix();
+
+  window.addEventListener('load', () => {
+
+  });
+});
