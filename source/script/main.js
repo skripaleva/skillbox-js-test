5951266994a08574ae4e6d23a22d742114a4f71c
Evgeniya <evgeniya_ignaten@mail.ru>
2024-03-05 23:12:38 +0300

    2 task addition

--- source/script/main.js
+++ source/script/main.js
@@ -1,6 +1,7 @@
 import "../scss/style.scss"
 import "./utils/prism.js";
 import {iosVhFix} from "./utils/ios-vh-fix.js";
+import "./modules/header/init-page-menu.js"
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
