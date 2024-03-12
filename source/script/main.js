4775eed1f0a70c4cb816c43682f2caea91b50180
Evgeniya <evgeniya_ignaten@mail.ru>
2024-03-12 10:33:32 +0300

    fix main

--- source/script/main.js
+++ source/script/main.js
@@ -1,13 +1,14 @@
 import "../scss/style.scss";
 import "./utils/prism.js";
 import {iosVhFix} from "./utils/ios-vh-fix.js";
-import "./modules/header/init-page-menu.js";
+import {burgerInit} from "./modules/header/init-page-menu.js";
 import {initFilter} from "./modules/filter/init-filter.js";
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
 
   window.addEventListener('load', () => {
+    burgerInit()
     if (window.location.pathname.split("/").pop() === "data.html")
       initFilter(window.location.hash);
   });
--- source/script/modules/header/init-page-menu.js
+++ source/script/modules/header/init-page-menu.js
@@ -4,48 +4,49 @@ const menu = document.querySelector('[data-main-nav="nav"]');
 const logo = document.querySelector('[data-header-logo="data-header-logo"]');
 const links = document.querySelectorAll('[data-nav-item="item"]');
 
+const burgerInit = () => {
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('is-active')
+        menu.classList.toggle('is-active')
+        body.classList.toggle('scroll-lock')
+        logo.classList.toggle('is-menu')
 
-burger.addEventListener('click', () => {
-    burger.classList.toggle('is-active')
-    menu.classList.toggle('is-active')
-    body.classList.toggle('scroll-lock')
-    logo.classList.toggle('is-menu')
+        let del = 0;
 
-    let del = 0;
+        links.forEach((item) => {
+            del += 150;
 
-    links.forEach((item) => {
-        del += 150;
+            setTimeout(() => {
+                item.classList.toggle('is-active');
+            }, del)
 
-        setTimeout(() => {
-            item.classList.toggle('is-active');
-        }, del)
+        });
+    })
 
-    });
-})
-
-const closeMenu = () => {
-    burger.classList.remove('is-active')
-    menu.classList.remove('is-active')
-    body.classList.remove('scroll-lock')
-    logo.classList.remove('is-menu')
-    links.forEach(item => item.classList.remove('is-active'))
-}
-
-burger.addEventListener("keydown", (e) => {
-    if (e.code === "Escape") {
-        console.log("Closing window...");
-        closeMenu()
+    const closeMenu = () => {
+        burger.classList.remove('is-active')
+        menu.classList.remove('is-active')
+        body.classList.remove('scroll-lock')
+        logo.classList.remove('is-menu')
+        links.forEach(item => item.classList.remove('is-active'))
     }
-});
 
-const ro = new ResizeObserver(entries => {
-    for(let entry of entries){
-        if (!entry.contentRect.width) {
+    burger.addEventListener("keydown", (e) => {
+        if (e.code === "Escape") {
+            console.log("Closing window...");
             closeMenu()
         }
-    }
-})
+    });
 
-ro.observe(burger)
+    const ro = new ResizeObserver(entries => {
+        for (let entry of entries) {
+            if (!entry.contentRect.width) {
+                closeMenu()
+            }
+        }
+    })
 
+    ro.observe(burger)
+}
 
+export {burgerInit}
